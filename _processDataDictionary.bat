213cead186419c85dec395a9d2c9152f54e71118
magp18 <marcopereira@gmx.ch>
2021-12-16 11:37:14 +0100

    added required bat scripts to use the DAK - processor; minor changes in the main excel file; added config file to pass to the dak processor

---
+++ _processDataDictionary.bat
@@ -0,0 +1,25 @@
+@ECHO off
+SET tooling_jar=tooling-1.3.1-SNAPSHOT-jar-with-dependencies.jar
+SET input_cache_path=%~dp0input-cache
+SET datadictionary_directory=input/l2
+REM the next 3 lines need to be edited for ANC
+SET datadictionary_filename=%datadictionary_directory%\Em Care - Stage 1 - Core Data Dictionary_2021-11-30.xlsx
+REM datadictionary_filename=%datadictionary_directory%\WHO-ANC.xlsx
+REM datadictionary_filename=%datadictionary_directory%\Em Care - Stage 1 - Core Data DictionaryMarcoRafael_v2.xlsx
+
+SET datadictionary_sheetname=EmCare.A. Registration
+SET scope=EmCare
+SET tooling=%input_cache_path%\%tooling_jar%
+SET upper_path=%%~dpx
+
+if EXIST "%tooling%" (
+	ECHO "%datadictionary_sheetname%"
+	JAVA -jar "%tooling%" -ProcessAcceleratorKit -s="%scope%" -pts="%datadictionary_filename%" -dep="%datadictionary_sheetname%" -op=./output -c="./processor_dak_conf.json"
+) ELSE (
+	if EXIST "%tooling%" (
+		JAVA -jar $tooling -ProcessAcceleratorKit -s=$scope -pts=./input/l2/"$datadictionary_filename" -dep="$datadictionary_sheetname" -op=.
+	) ELSE (
+		ECHO [91m ProcessAcceloratorKit NOT FOUND in input-cache or parent folder.  Please run _updateCQFTooling.  Aborting...
+		ECHO [0m
+    )
+)
